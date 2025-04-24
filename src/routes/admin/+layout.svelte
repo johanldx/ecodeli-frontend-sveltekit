5d01c851e8163ff90bb8363ce9e1f7ce856f9017
Johan Ledoux <johan.ledoux25@gmail.com>
2025-04-24 15:48:13 +0200

    dev back office

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -8,6 +8,7 @@
 const users_title = tStatic('admin.global.users_title');
 const addresses_title = tStatic('admin.global.addresses_title');
 const langs_title = tStatic('admin.global.langs_title');
+const ads_title = tStatic('admin.global.ads_title');
 
   let { children } = $props();
 
@@ -15,6 +16,7 @@ const langs_title = tStatic('admin.global.langs_title');
     { name: users_title, url: "/admin/users" },
     { name: addresses_title, url: "/admin/addresses" },
     { name: langs_title, url: "/admin/langs" },
+    { name: ads_title, url: "/admin/ads" },
   ];
 </script>
 
---
+++ src/routes/admin/ads/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { tabTitle } from "$lib/utils/tabTitle";
+	import { onDestroy, onMount } from "svelte";
+
+	onMount(() => {onDestroy(tabTitle('admin.ads'));});
+</script>
+
+Gestion des annonces
