1e0cefd3c6d9dc34f18bd8dfbd901ef84a68fdc5
Johan Ledoux <johan.ledoux25@gmail.com>
2025-07-03 18:11:15 +0200

    fix

--- start.js
+++ start.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 
-// Variables d'environnement
 process.env.PORT = '3001';
 process.env.HOST = '0.0.0.0';
 process.env.NODE_ENV = 'production';
 
-import('./server.js');
+import('./build/index.js'); 
